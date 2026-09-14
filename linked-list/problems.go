b4b2319a6305c33d35fa5788753563fc0d7f943f
Aashish <aashishdubey.dev@gmail.com>
2026-09-14 18:21:03 +0530

    add linkedlist Reverse MiddleNode HasCycle problem

--- linked-list/problems.go
+++ linked-list/problems.go
@@ -1,33 +1,58 @@
 package linkedlist
 
-import ()
+func ReversList(linkedlist *LinkedList) {
+	var prev *Node
+	curr := linkedlist.Head
 
-func ReverseList(list *LinkedList) {
-
-	prev := (*Node)(nil)
-	curr := list.Head
-
-	for curr != nil {
+	for curr.Next != nil {
 		next := curr.Next
 		curr.Next = prev
 		prev = curr
 		curr = next
 	}
+	linkedlist.Head = prev
 
-	list.Head = prev
+}
+
+func MiddleNode(list *LinkedList) *Node {
+	slow := list.Head
+	fast := list.Head
+
+	for fast != nil && fast.Next != nil {
+		slow = slow.Next
+		fast = fast.Next.Next
+	}
+	return slow
+}
+
+func HasCycle(list *LinkedList) bool {
+	slow := list.Head
+	fast := list.Head
+
+	for fast != nil && fast.Next != nil {
+		slow = slow.Next
+		fast = fast.Next.Next
+
+		if slow == fast {
+			return true
+		}
+	}
+	return false
 }
 
 func RunProblems() {
 
 	list := LinkedList{}
 	list.InsertAtHead(5)
-	list.InsertAtHead(29)
-	list.InsertAtHead(20)
-	list.InsertAtTail(30)
+	list.InsertAtHead(6)
+	list.InsertAtHead(7)
+	list.InsertAtHead(8)
 
 	list.Print()
 
-	ReverseList(&list)
+	// ReversList(&list)
+	// list.Print()
 
-	list.Print()
+	MiddleNode(&list)
+
 }
